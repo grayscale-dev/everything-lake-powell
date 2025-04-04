4ba6bcc0677568fa8b2478e2a41f3a6936863c1f
zbrown001 <zbrown@nanolos.com>
2025-04-03 18:40:59 -0600

    add the widget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,17 @@ import { CurrentTempService } from './core/services/current-temp.service';
 import { CommonModule } from '@angular/common';
 import { SkeletonModule } from 'primeng/skeleton';
 import { TableModule } from 'primeng/table';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, SkeletonModule, TableModule],
+  imports: [RouterOutlet, CommonModule, SkeletonModule, TableModule, FontAwesomeModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  faArrowUp = faArrowUp;
   currentTempService = inject(CurrentTempService)
   products!: any[];
 
