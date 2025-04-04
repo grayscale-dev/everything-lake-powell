09bf2254c1a349415bc703114850049acb9af5a6
zbrown001 <zbrown@nanolos.com>
2025-04-04 14:16:47 -0600

    new theme and data points

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,17 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
 import { CurrentTempService } from './core/services/current-temp.service';
 import { CommonModule } from '@angular/common';
 import { SkeletonModule } from 'primeng/skeleton';
 import { TableModule } from 'primeng/table';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, SkeletonModule, TableModule, FontAwesomeModule],
+  imports: [CommonModule, SkeletonModule, TableModule, FontAwesomeModule],
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-  faArrowUp = faArrowUp;
+export class AppComponent {
+  math = Math;
   currentTempService = inject(CurrentTempService)
-  products!: any[];
-
-  ngOnInit() {
-      this.products = Array.from({ length: 5 }).map((_, i) => `Item #${i}`);
-  }
 }
--- src/app/core/services/current-temp.service.ts
+++ src/app/core/services/current-temp.service.ts
@@ -4,7 +4,7 @@ import { Injectable, resource } from "@angular/core";
 export class CurrentTempService {
     currentTemp = resource({
         loader: async (params) => {
-            const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=36.9147&longitude=-111.4558&current=temperature_2m&forecast_days=1&temperature_unit=fahrenheit')
+            const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=36.9147&longitude=-111.4558&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum&hourly=temperature_2m,uv_index&current=temperature_2m&past_days=14&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch')
             if (!response.ok) {
                 throw new Error('Failed to fetch the Current Temp')
             }
