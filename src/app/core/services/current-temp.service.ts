fe498e3eca877a861d9aa048f48119693dcb0118
zbrown001 <zbrown@nanolos.com>
2025-04-04 17:52:04 -0600

    make uv_index live

--- src/app/core/services/current-temp.service.ts
+++ src/app/core/services/current-temp.service.ts
@@ -7,7 +7,7 @@ export class CurrentTempService {
   currentTemp = resource({
     loader: async () => {
       const { latitude, longitude } = this.coordinates;
-      const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum&hourly=temperature_2m,uv_index,weather_code&current=temperature_2m&past_days=14&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
+      const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum&hourly=temperature_2m,uv_index,weather_code&current=uv_index,temperature_2m&past_days=14&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error('Failed to fetch the Current Temp');
